69792b14462cf80ef10cc989c65a6b828e2c6603
05sachin <sachingangwar137@gmail.com>
2023-12-24 09:47:08 +0530

    styling

--- src/components/Body.js
+++ src/components/Body.js
@@ -31,10 +31,11 @@ const RestaurantList = ({restaurants})=>{
 const Body = ()=>{
     const [searchText ,setSearchText] = useState("");
     const [restaurants,setRestaurants] = useState([]);
-    const [toggle,setToggle] = useState(false);
+    const [filterRestaurants,setFilterRestaurants] = useState([]);
+   
     useEffect(()=>{
         fetchData();
-    },[toggle]);
+    },[]);
     {console.log("hello")}
     const fetchData = async ()=>{
         const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.8466937&lng=80.94616599999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
@@ -47,20 +48,21 @@ const Body = ()=>{
             const info = ele?.card?.card?.gridElements?.infoWithStyle?.restaurants;
             if(info){
                 setRestaurants(info);
+                setFilterRestaurants(info);
                 break;
             } 
         }
 
     }
     // console.log(restaurants.length);
-    return (restaurants.length===0)?<Shimmer/>:(
+    return (filterRestaurants.length===0)?<Shimmer/>:(
         
     <div className="body">
         
         <div className="filter"> 
-            <button onClick={()=>{
+            <button className="top-rated-btn" onClick={()=>{
                     const data = topRatedRestaurants(restaurants);
-                    setRestaurants(data);
+                    setFilterRestaurants(data);
                 }   
             }> Top Rated Restaurants
             </button>
@@ -74,19 +76,17 @@ const Body = ()=>{
             
                 <button className="search-btn"
                 onClick={()=>{
-                    if(searchText && searchText.trim().length!=0){
-                        // need to filter data
-                        const data = filterData(restaurants,searchText);
-                        // update the state - restaurants
-                        setRestaurants(data);
-                    }else {
-                        setToggle(!toggle);
-                    }
+                
+                    // need to filter data
+                    const data = filterData(restaurants,searchText.trim());
+                    // update the state - restaurants
+                    setFilterRestaurants(data);
+                   
                 }}>Search</button>
             </div>
         </div>
         
-        <RestaurantList restaurants={restaurants} />
+        <RestaurantList restaurants={filterRestaurants} />
     </div>);
 };
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
+import { useState } from "react";
 export const Title = ()=>(
     <a href="/">
         <img id="logo" alt="logo" src="https://www.shutterstock.com/shutterstock/photos/1328492696/display_1500/stock-vector-food-finder-app-logo-design-vector-template-1328492696.jpg"></img>
     </a>
 );
-const Header = ()=>(
+const Header = ()=>{
+    const [loginBtn,setLoginBtn] = useState("LogIn");
+    return (
     <div className="header">
         <Title/>
         <div className="nav-item">
@@ -13,9 +16,17 @@ const Header = ()=>(
                 <li>Contact</li>
                 <li>Cart</li>
             </ul>
+            <button className="login-btn"
+            onClick={()=>{
+                if(loginBtn==="LogIn"){
+                    setLoginBtn("LogOut");
+                }else{
+                    setLoginBtn("LogIn");
+                }
+            }}>{loginBtn}</button>
         </div>
-        <button className="login-btn">LogIn</button>
-    </div>
-);
+        
+    </div>);
+};
 
 export default Header;
