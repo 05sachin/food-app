84e03c933866dcf266aaa36976cde8d703590482
05sachin <sachingangwar137@gmail.com>
2023-12-27 18:38:32 +0530

    restaurant menu

--- src/components/MenuList.js
+++ src/components/MenuList.js
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import { menuImg } from "../utils/config";
 import { Icon } from '@iconify/react';
-const MenuCard = ({name,price,imageId,isVeg})=>{
+const MenuCard = ({name,price,imageId,isVeg,defaultPrice})=>{
     
     return (
         <div className="menu-card">
             <div className="left-part">
                 { isVeg ?(<Icon icon="mdi:lacto-vegetarian" />):(<img src="https://i.pinimg.com/474x/14/0b/0e/140b0e8a911d1734c496155aa97a56a8.jpg" width={"20px"} height={"20px"}></img>)}
                 <h4>{name}</h4>
-                <p > <Icon icon="mdi:rupee" /> <span>{price/100}</span></p>
+                <p > <Icon icon="mdi:rupee" /> <span>{(price)?price/100:defaultPrice/100}</span></p>
             </div>
             <div className="right-part">
                {!imageId?(<img className="no-img"></img>):(
