5bcc1333b31f77a9f807d6f1b8677bf51440cdf0
05sachin <sachingangwar137@gmail.com>
2024-01-01 18:16:51 +0530

    styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,26 +10,27 @@ const Header = ()=>{
     const [loginBtn,setLoginBtn] = useState("LogIn");
     const onlineStatus = useOnlineStatus();
     return (
-    <div className="header">
-        <Title/>
-        <div className="nav-item">
-            <ul>
-                <li >Online Status:{ onlineStatus?'✅':'🔴' }</li>
-                <li><Link className="link" to={"/"}>Home</Link></li>
-                <li><Link className="link" to={"/about"}>About</Link></li>
-                <li><Link className="link" to={"/contact"}>Contact</Link></li>
-                <li><Link className="link" to={"/cart"}>Cart</Link></li>
-            </ul>
-            <button className="login-btn"
-            onClick={()=>{
-                if(loginBtn==="LogIn"){
-                    setLoginBtn("LogOut");
-                }else{
-                    setLoginBtn("LogIn");
-                }
-            }}>{loginBtn}</button>
+    <div className="header-wrapper">
+        <div className="header">
+            <Title/>
+            <div className="nav-item">
+                <ul>
+                    <li >Online Status:{ onlineStatus?'✅':'🔴' }</li>
+                    <li><Link className="link" to={"/"}>Home</Link></li>
+                    <li><Link className="link" to={"/about"}>About</Link></li>
+                    <li><Link className="link" to={"/contact"}>Contact</Link></li>
+                    <li><Link className="link" to={"/cart"}>Cart</Link></li>
+                </ul>
+                <button className="login-btn"
+                onClick={()=>{
+                    if(loginBtn==="LogIn"){
+                        setLoginBtn("LogOut");
+                    }else{
+                        setLoginBtn("LogIn");
+                    }
+                }}>{loginBtn}</button>
+            </div>
         </div>
-        
     </div>);
 };
 
--- src/components/RestaurantMenuCard.js
+++ src/components/RestaurantMenuCard.js
@@ -5,7 +5,7 @@ const RestaurantMenuCard = ({name,price,imageId,isVeg,defaultPrice,description})
     return (
         <div className="menu-card">
             <div className="left-part">
-                { isVeg ?(<Icon icon="mdi:lacto-vegetarian" />):(<img src="https://i.pinimg.com/474x/14/0b/0e/140b0e8a911d1734c496155aa97a56a8.jpg" width={"20px"} height={"20px"}></img>)}
+                { isVeg ?(<Icon icon="mdi:lacto-vegetarian" color="green" />):(<Icon icon="mdi:lacto-vegetarian" color="red" />)}
                 <h4>{name}</h4>
                 <p > <Icon icon="mdi:rupee" /> <span>{(price)?price/100:defaultPrice/100}</span></p>
                 <p className="description">{description}</p>
