da7e9b1daed3f8843afb7aa5504d80d75e76d9fb
05sachin <sachingangwar137@gmail.com>
2024-01-02 03:22:17 +0530

    contact && error page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,31 @@
+import { useState } from "react";
+
 const Contact = ()=>{
-    return (
+    const [toggle,setToggle] = useState(false);
+    const handle = ()=>{
+        setToggle(true);
+    }
+    return toggle?(<div className="contact">
+        <p>Thanks for your valuable feedback!</p>
+    </div>):
+    (
         <div className="contact">
-            <h1>Contact Us</h1>
+            <h2>Contact Us</h2>
+            <form className="form" onSubmit={handle}>
+                <label for="fname">First Name:</label>
+                <input type="text" placeholder="Enter First Name" required></input><br/>
+
+                <label for="lname">Last Name:</label>
+                <input type="text" placeholder="Enter Last Name" required></input><br/>
+
+                <label for="email" >Your Email:</label>
+                <input type="email" placeholder="Enter Email" required></input><br/>
+
+                <label for="message">Message:</label>
+                <textarea  required></textarea><br/>
+                
+                <button className="submitBtn" type="submit">Send Message</button>
+            </form>
         </div>
     );
 }
--- src/components/Error.js
+++ src/components/Error.js
@@ -1,13 +1,20 @@
-import { useRouteError } from "react-router-dom";
+import { useRouteError,Link } from "react-router-dom";
 const Error = ()=>{
     const error = useRouteError();
     console.log(error)
     return (
-        <div className="error">
-            <h1>Oops!!!</h1>
-            <h2>Something went wrong!!</h2>
-            <h3>{error.status} : {error.statusText}</h3>
-        </div>
+       <div className="error-wrapper">
+            <div className="error">
+                <h1>Oops!!!</h1>
+                <h2>Something went wrong!!</h2>
+                <h3>{error.status} : Page {error.statusText}</h3>
+                <br></br>
+                <p>
+                 Looks like you've followed a broken link or entered a URL that doesn't exist on this site.
+                </p>
+                <Link to={"/"} className="errorBtn">Go Back</Link>
+            </div>
+       </div>
     );
 };
 
